9916972377df062947deff822f4bc3100fd6c500
HryndiiAndrii <78363192+HryndiiAndrii@users.noreply.github.com>
2021-04-07 22:58:26 +0300

    Adding DRF API for author

--- library/library/urls.py
+++ library/library/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('authentication.urls')),
+    path('api/v1/author/', include('author.urls')),
 ]
