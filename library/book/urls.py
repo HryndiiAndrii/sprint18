d2df5917198568f0caade422066060082c80e463
HryndiiAndrii <78363192+HryndiiAndrii@users.noreply.github.com>
2021-04-07 23:51:41 +0300

    Adding DRF API for book

---
+++ library/book/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+
+from . import views
+
+router = routers.DefaultRouter()
+router.register('', views.BookView)
+
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- library/library/urls.py
+++ library/library/urls.py
@@ -17,10 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('authentication.urls')),
     path('api/v1/author/', include('author.urls')),
+    path('api/v1/book/', include('book.urls')),
 ]
